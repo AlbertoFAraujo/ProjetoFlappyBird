b33f6dfe75e06ad786a8e9261b1934f73419d347
Alberto Araujo <105552990+AlbertoFAraujo@users.noreply.github.com>
2023-02-27 00:15:25 -0300

    Início de projeto

---
+++ FlappyBird.py
@@ -0,0 +1,50 @@
+import pygame
+import os
+import random
+
+# Definição de constantes
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+# Definir as imagens do jogo
+# pygame.image.load >>> carregar a imagem
+# os.path.join >>> juntar o caminho da pasta de imagens
+# pygame.transform.scale2x >>> Ajustar a imagem
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGENS_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+]
+
+# Fonte que herdará a pontuação do jogo
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+class Passaro:
+    IMGS = IMAGENS_PASSARO
+    # Animações da Rotação
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __int__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.
+
+
+
+
+    pass
+
+class Cano:
+    pass
+
+class Chao:
