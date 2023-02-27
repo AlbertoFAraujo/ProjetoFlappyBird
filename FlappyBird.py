2305bac99e8f3cade44052e4beec481f9f1f778a
Alberto Araujo <105552990+AlbertoFAraujo@users.noreply.github.com>
2023-02-27 13:00:41 -0300

    Estrutura base de background e movimentos do pássaro

--- FlappyBird.py
+++ FlappyBird.py
@@ -23,6 +23,7 @@ IMAGENS_PASSARO = [
 pygame.font.init()
 FONTE_PONTOS = pygame.font.SysFont('arial', 50)
 
+
 class Passaro:
     IMGS = IMAGENS_PASSARO
     # Animações da Rotação
@@ -38,16 +39,61 @@ class Passaro:
         self.altura = self.y
         self.tempo = 0
         self.contagem_imagem = 0
-        self.imagem = IMGS[0]
+        self.imagem = self.IMGS[0]
+
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        # calcular o deslocament
+        self.tempo += 1
+        deslocamento = 1.5 + (self.tempo ** 2) + self.velocidade + self.tempo
+
+        # restringir o deslogamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        # o ângulo do pássaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+
+        else:
+            if self.angulo > -90:
+                self.angulo -= self.VELOCIDADE_ROTACAO
+
+    def desenhar(self):
+        # definir qual imagem do passaro vai usar
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO * 4 + 1:
+            self.imagem = self.IMGS[0]
 
 
+        # se o passaro tiver caindo eu não vou bater asa
 
 
+        # desenhar imagem
 
 
-    pass
 
 class Cano:
     pass
 
+
 class Chao:
+    pass
